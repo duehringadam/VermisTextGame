bad86cea9bec437b94d034565df472fbed1913d3
Adam Duehring <duehring.adam@gmail.com>
2024-10-25 11:29:49 -0400

    Delete Godot directory
    updating the repo

--- Godot/items/item.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-class_name Item
-
-@export var itemName: String = "Item Name"
-@export var itemType := Types.ItemTypes.KEY
-@export var itemDescription: String = "Item Description"
-
-var useValue = null
-
